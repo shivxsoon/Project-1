18325d631458fa666b3f2ac3303a1baa460b8620
shivxsoon <mshivaryashashank@gmail.com>
2025-08-11 19:51:03 +0800

    Day 8 - Python
    Day 8 - Python

--- Project 1.py
+++ Project 1.py
@@ -1,38 +1,8 @@
-score = 0
+string = input("Please enter your own string : ", shiv )
+string2 = ('')
 
-answer = input("Who is the current captain of the India cricket team? ")
-if answer.lower() == "rohit sharma":
-    print("Correct!")
-    score += 1
-else:
-    print(f"The correct answer is 'Rohit Sharma', not {answer!r}")
-
-answer = input("Which Indian player is known as the 'King Kohli'? ")
-if answer.lower() == "virat kohli":
-    print("Correct!")
-    score += 1
-else:
-    print(f"The correct answer is 'Virat Kohli', not {answer!r}")
-
-answer = input("Which stadium is also called the 'Home of Indian Cricket'? ")
-if answer.lower() == "wankhede stadium" or answer.lower() == "wankhede":
-    print("Correct!")
-    score += 1
-else:
-    print(f"The correct answer is 'Wankhede Stadium', not {answer!r}")
-
-answer = input("Who is the highest run scorer in ODI cricket for India? ")
-if answer.lower() == "sachin tendulkar":
-    print("Correct!")
-    score += 1
-else:
-    print(f"The correct answer is 'Sachin Tendulkar', not {answer!r}")
-
-answer = input("Which Indian bowler is known for his yorkers and death overs? ")
-if answer.lower() == "jasprit bumrah":
-    print("Correct!")
-    score += 1
-else:
-    print(f"The correct answer is 'Jasprit Bumrah', not {answer!r}")
-
-print(f"\nYour final score is {score}/5")
+for i in string :
+    string2 = i + string2
+    
+print("\nThe Original String =", string)
+print("Reversed String =", string2)
