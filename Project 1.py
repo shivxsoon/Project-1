1c32d31005ff553b561c742cd86ae1aa677e6f3a
shivxsoon <mshivaryashashank@gmail.com>
2025-08-13 19:16:05 +0800

    Day 9-Python
    Python Day -9

--- Project 1.py
+++
@@ -1,8 +0,0 @@
-string = input("Please enter your own string : ", shiv )
-string2 = ('')
-
-for i in string :
-    string2 = i + string2
-    
-print("\nThe Original String =", string)
-print("Reversed String =", string2)
